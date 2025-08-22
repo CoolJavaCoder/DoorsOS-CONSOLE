601f85e2cb091f2d7cbff433ba772ccc1b048fdf
BeCharlie32 <BeCharlie32@outlook.com>
2025-08-23 08:28:36 +1000

    Working file system, just can't edit files yet

--- main.cpp
+++ main.cpp
@@ -2,13 +2,15 @@
 #include <string>
 #include <cstdlib>
 
+
 using namespace std;
+void fileManager();
 
 int performAction(string action) {
     if (action == "help") {
         cout << "Welcome to the Doors Operating System. Here are some commands you can use:" << endl;
         cout << " - help: Show this help message" << endl;
-        cout << " - exit: Exit the program" << endl;
+        cout << " - exit: Exit the OS" << endl;
         cout << " - version: Show the version information" << endl;
         cout << " - app library: Show the available applications and use them." << endl;
     } else if (action == "exit") {
@@ -25,7 +27,12 @@ int performAction(string action) {
         string appChoice;
         getline(cin, appChoice);
         cout << "You have chosen: " << appChoice << endl;
-        appChoice();
+        if (appChoice == "File Manager") {
+            fileManager();
+        }
+        else {
+            cout << "Unknown application: " << appChoice << endl;
+        }
     } else {
         cout << "Unknown command: " << action << endl;
     }
@@ -61,3 +68,61 @@ int main() {
     return 0;
 
 }
+void fileManager() {
+    cout << "DOORS FILES" << endl;
+    cout << "You can store up to ten files here." << endl;
+    string fileAction;
+    string files[10];
+    string file1;
+    string file2;
+    string file3;
+    string file4;
+    files[0] = file1;
+    files[1] = file2;
+    files[2] = file3;
+    files[3] = file4;
+
+    while (true) {
+        cout << "Enter an action to do with your files (new, edit, delete, list, exit)" << endl;
+        getline(cin, fileAction);
+        if(fileAction == "new"){
+               cout << "Enter the name for the new file: ";
+               string newFileName;
+               getline(cin, newFileName);
+               for (int i = 0; i < 6; i++) {
+                   if (files[i].empty()) {
+                       files[i] = newFileName;
+                       cout << "File created: " << newFileName << endl;
+                       break;
+                   }
+               }
+        } else if(fileAction == "list"){
+            cout << "Listing files:" << endl;
+            cout << " - " << files[0] << endl;
+            cout << " - " << files[1] << endl;
+            cout << " - " << files[2] << endl;
+            cout << " - " << files[3] << endl;
+            cout << " - " << files[4] << endl;
+            cout << " - " << files[5] << endl;
+            cout << " - " << files[6] << endl;
+            cout << " - " << files[7] << endl;
+            cout << " - " << files[8] << endl;
+            cout << " - " << files[9] << endl;
+        } else if(fileAction == "delete"){
+            cout << "Enter the name of the file you want to delete: ";
+            string deleteFileName;
+            getline(cin, deleteFileName);
+            for (int i = 0; i < 10; i++) {
+                if (files[i] == deleteFileName) {
+                    files[i].clear();
+                    cout << "File deleted: " << deleteFileName << endl;
+                    break;
+                }
+            }
+        } else if(fileAction == "exit"){
+            cout << "Exiting file manager." << endl;
+            break;
+        }
+    }
+    
+}    
